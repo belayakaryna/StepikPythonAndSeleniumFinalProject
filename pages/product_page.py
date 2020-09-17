0ae9a4caeb6692849211cab7da2786abd4533e85
Karina Belaya <belaya.karina1998@gmail.ru>
2020-09-17 20:45:06 +0300

    add product_page

---
+++ pages/product_page.py
@@ -0,0 +1,27 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_BUTTON), "Add button is not presented"
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
+        add_button.click()
+
+    def should_be_book_price(self):
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
+        basket_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE_IN_BASKET).text
+        assert  book_price == basket_price, "Price don't match"
+
+    def should_be_book_name(self):
+        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+        book_basket = self.browser.find_element(*ProductPageLocators.BOOK_NAME_IN_BASKET).text
+        assert book_name == book_basket, "Name don't match"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+           "Success message is presented, but should not be"
+
+    def is_dissappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+           "Success message is presented, but should not be"

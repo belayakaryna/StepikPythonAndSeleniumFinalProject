466966d5f3a77c0128c1209d1d844aff5986b449
Karina Belaya <belaya.karina1998@gmail.ru>
2020-09-18 06:21:42 +0300

    group tests and setup

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.by import By
 from .locators import BasketPageLocators
 
 class BasketPage(BasePage):
- def should_be_basket_empty_message(self):
-    assert self.is_not_element_present(*BasketPageLocators.EMPTY_BASKET), "Item in basket"
+    def should_be_basket_empty_message(self):
+        assert self.is_not_element_present(*BasketPageLocators.EMPTY_BASKET), "Item in basket"
 
- def is_basket_empty(self):
-    assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), "Basket is not empty"
+    def is_basket_empty(self):
+        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), "Basket is not empty"
--- test_product_page.py
+++ test_product_page.py
@@ -17,55 +17,82 @@ import time
                                   #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        self.login_page = LoginPage(browser, link)
+        self.login_page.open()
+        count = random.randint(1, 100)
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time() + count)
+        self.login_page.register_new_user(email, password)
+        self.login_page.should_be_authorized_user()
+    def test_user_cant_see_success_message(self, browser,link):
+        #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        self.product_page = ProductPage(browser, link)
+        self.product_page.open()
+        self.product_page.should_not_be_success_message()
+  
+    def test_user_can_add_product_to_basket(self, browser,link):
+        #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        self.product_page = ProductPage(browser, link)
+        self.product_page.open()
+        self.product_page.add_product_to_basket()
+        self.product_page.solve_quiz_and_get_code()
+        self.product_page.should_be_book_price()
+        self.product_page.should_be_book_name()
 
-def test_guest_can_add_product_to_basket(browser,link):
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_product_to_basket()
     product_page.solve_quiz_and_get_code()
-    #time.sleep(10)
+    
     product_page.should_be_book_price()
     product_page.should_be_book_name()
 @pytest.mark.xfail(reason="wrong message")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser): 
-     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-     product_page = ProductPage(browser, link)
-     product_page.open()
-     product_page.add_product_to_basket()
-     product_page.solve_quiz_and_get_code()
-     product_page.should_not_be_success_message()
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.solve_quiz_and_get_code()
+    product_page.should_not_be_success_message()
 def test_guest_cant_see_success_message(browser): 
-     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-     product_page = ProductPage(browser, link)
-     product_page.open()
-     product_page.should_not_be_success_message()
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_not_be_success_message()
 @pytest.mark.xfail(reason="not disappiring")
 def test_message_disappeared_after_adding_product_to_basket(browser): 
-     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-     product_page = ProductPage(browser, link)
-     product_page.open()
-     product_page.add_product_to_basket()
-     product_page.solve_quiz_and_get_code()
-     product_page.is_dissappeared()
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.solve_quiz_and_get_code()
+    product_page.is_dissappeared()
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
+
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-     page = ProductPage(browser, link)
-     page.open()
-     page.should_enter_basket()
-     page2 = BasketPage(browser, browser.current_url)
-     page2.is_basket_empty()
-     page2.should_be_basket_empty_message()
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_enter_basket()
+    page2 = BasketPage(browser, browser.current_url)
+    page2.is_basket_empty()
+    page2.should_be_basket_empty_message()
      
 	 
 	 

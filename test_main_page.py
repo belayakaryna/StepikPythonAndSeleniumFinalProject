b83cfe8d81e2230ed00d801ff242a4d5234f7519
Karina Belaya <belaya.karina1998@gmail.ru>
2020-09-18 06:57:47 +0300

    end of the progect

--- test_main_page.py
+++ test_main_page.py
@@ -16,7 +16,7 @@ class TestLoginFromMainPage():
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
-    def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
--- test_product_page.py
+++ test_product_page.py
@@ -33,7 +33,7 @@ class TestUserAddToBasketFromProductPage():
         self.product_page = ProductPage(browser, link)
         self.product_page.open()
         self.product_page.should_not_be_success_message()
-  
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser,link):
         #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         self.product_page = ProductPage(browser, link)
@@ -42,7 +42,7 @@ class TestUserAddToBasketFromProductPage():
         self.product_page.solve_quiz_and_get_code()
         self.product_page.should_be_book_price()
         self.product_page.should_be_book_name()
-
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     product_page = ProductPage(browser, link)
@@ -78,13 +78,13 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
